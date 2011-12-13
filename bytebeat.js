6e7e656834667d4da7de2efc8b17179daa662963
Darius Bacon <darius@wry.me>
2011-12-12 23:34:38 -0800

    Keep the waveform from jumping around so randomly

--- bytebeat.js
+++ bytebeat.js
@@ -160,6 +160,7 @@ function vizStop() {
 
 function updateViz(canvas, audio, sound) {
     var t = Math.round(audio.currentTime * sound.rate);
+    t = (t + 0xFF) & ~0xFF; // Keep the line from jumping around so randomly
     if (prev_t === t)
         return;            // Player probably paused, don't waste CPU.
     var T = sound.duration * sound.rate;
