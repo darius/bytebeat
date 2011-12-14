c297b501dd935112d0b48f322ee4758720b54051
Darius Bacon <darius@wry.me>
2011-12-14 00:49:03 -0800

    Example-converter: Handle stereo formulas. Tweak explanatory text.

--- convert_collection.py
+++ convert_collection.py
@@ -1,14 +1,19 @@
+import re
+
+## mu = 'L: ((t&4096)?((t*(t^t%255)|(t>>4))>>1):(t>>3)|((t&8192)?t<<2:t)) R: t*(((t>>9)^((t>>9)-1)^1)%13)'
+## m = re.match(r'L: (.*) R: (.*)',  mu)
+## m.groups()[1]
+#. 't*(((t>>9)^((t>>9)-1)^1)%13)'
+
 text = open('music_formula_collection.txt').read()
 
 def main():
-    print """This is an automated markup of the <a
+    print """This is an automated mark-up of the <a
 href="http://pelulamu.net/countercomplex/music_formula_collection.txt">
-collection of oneliner music formulas</a> version 2011-10-18,
+collection of oneliner music formulas</a> (version 2011-10-18)
 by viznut, Converted by <code>convert_collection.py</code> at
 <a href="https://github.com/darius/bytebeat">https://github.com/darius/bytebeat</a>.
-The conversion is bound to have got some things wrong: e.g. the one
-stereo entry is certainly broken because I haven't coded a converter
-for that case.  '"""
+(The conversion is bound to have gotten at least a few things wrong.)'"""
     for head, body in chunk_input(): 
         print '<h2>%s</h2>' % head.replace('=', '').strip()
         print '<ul>'
@@ -28,26 +33,30 @@ for that case.  '"""
                 else:
                     assert False
             if rest.endswith('kHz'):
-                rate = rest[:-3].split()[-1]
+                parts = rest[:-3].split()
+                rate, rest = parts[-1], ' '.join(parts[:-1])
                 rate = {'44': 44.1, '11': 11.025}.get(rate, rate)
                 rate = int(float(rate) * 1000)
-                rest = ' '.join(rest[:-3].split()[:-1])
+            if rest.endswith('stereo'):
+                 rest = rest[:-len('stereo')].strip()
+                 code0, code1 = re.match(r'L: (.*) R: (.*)', formula).groups()
+            else:
+                code0, code1 = formula, None
             print '<li>'
             if url:
                 print '<a href="%s">(source)</a> | ' % url
-            print format_row(formula, author, date, url, rate, rest)
+            print format_row(code0, code1, author, date, url, rate, rest)
         print '</ul>'
 
 import cgi, urllib
 
-def format_row(formula, author, date, url, rate, rest):
+def format_row(code0, code1, author, date, url, rate, rest):
     title = ' '.join(filter(None, [author, date, rest]))
     title = title or formula
-    params = dict(code0=formula, title=title)
-    if rate is not None:
-        params['samplerate'] = str(rate)
-    if url is not None:
-        params['source'] = url
+    params = dict(code0=code0, title=title)
+    if code1 is not None: params['code1'] = code1
+    if rate is not None:  params['samplerate'] = str(rate)
+    if url is not None:   params['source'] = url
     return ('<a href="http://wry.me/toys/bytebeat/?%s">%s</a>'
             % (my_urlencode(params), cgi.escape(title)))
 
